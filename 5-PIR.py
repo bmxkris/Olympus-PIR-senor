8c767c3487c1a842a195456e22d3dbed67c42a6f
k.flint <kristian.flint@net-a-porter.com>
2020-05-18 12:49:57 +0100

    Working script for RPI that tirggers an Olympus camera using a PIR

--- 5-PIR.py
+++ 5-PIR.py
@@ -6,19 +6,35 @@ from gpiozero import MotionSensor
 from datetime import datetime
 import time
 import os
+import subprocess
 
-datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
-print("Starting PIR.py")
+camera_SSID = ""
+wifi_sleep_time = 10
+camera_interval_time = 5
 
+print( datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)") + " Starting PIR.py")
 
+# do we have the network available?
+print( datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)") + " Checking camera is available")
+
+try:
+    while subprocess.check_output(['sudo', 'iwgetid']).split('"')[1] != camera_SSID:
+        print (datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)") + " WiFi network not found: " + subprocess.check_output(['sudo', 'iwgetid']).split('"')[1])
+        # Wait for 5 seconds
+        time.sleep(wifi_sleep_time)
+
+except Exception, e:
+    print e
+
+print( datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)") + " Camera found, sending set up command")
 os.system( "curl 192.168.0.10/switch_cammode.cgi?mode=shutter")
 # Set a variable to hold the GPIO Pin identity
 pir = MotionSensor(17)
 
-print("Waiting for PIR to settle")
+print( datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)") + " Waiting for PIR to settle")
 pir.wait_for_no_motion()
 
-print("PIR Module Test (CTRL-C to exit)")
+print( datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)") + " Olympus PIR Module (CTRL-C to exit)")
 
 # Variables to hold the current and last states
 currentstate = False
@@ -32,23 +48,26 @@ try:
 
         # If the PIR is triggered
         if currentstate == True and previousstate == False:
-            print("    Motion detected!")
-            # cmd = "git --version"
-            # returned_value = os.system(cmd)  # returns the exit code in unix
-            # print('returned value:', returned_value)
+            
+            print( datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)") + "    Motion detected!")
 
-            camera_command = "curl 192.168.0.10/exec_shutter.cgi?com=1st2ndpush && sleep 3 && curl 192.168.0.10/exec_shutter.cgi?com=2nd1strelease"
-            os.system(camera_command)
+            print( datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)") + "    Push!")
+            os.system( "curl 192.168.0.10/exec_shutter.cgi?com=1st2ndpush")
+
+            time.sleep(camera_interval_time)
+
+            print( datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)") + "    Release!")
+            os.system("curl 192.168.0.10/exec_shutter.cgi?com=2nd1strelease")
 
             # Record previous state
             previousstate = True
         # If the PIR has returned to ready state
         elif currentstate == False and previousstate == True:
-            print("    No Motion")
+            print( datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)") + "    No Motion")
             previousstate = False
 
         # Wait for 10 milliseconds
         time.sleep(0.01)
 
 except KeyboardInterrupt:
-    print("    Quit")
+    print( datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)") + "    Quit")
